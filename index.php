a7898e39173957a404d5c22970c729581626edbf
rich5k <richardanatsui@gmail.com>
2020-09-26 14:35:05 +0100

    added html tags

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Welcome to my course about php!!
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>

61811d5885051d690f52ebda6da53a1bc24e5bd7
rich5k <richardanatsui@gmail.com>
2020-10-06 00:49:03 +0100

    created a form

--- contact/index.php
+++ contact/index.php
@@ -6,6 +6,10 @@
     <title>Contact</title>
 </head>
 <body>
-    
+    <h1>Get in touch</h1>
+    <p>Please fill in the fields</p>
+    <form action="contact.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+    </form>
 </body>
 </html>

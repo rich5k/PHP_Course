7e1a41576a1e00e29bcbbb0cc34995ab585c8c04
rich5k <richardanatsui@gmail.com>
2020-10-06 00:58:55 +0100

    added email, subject, message fields and submit button to form

--- contact/index.php
+++ contact/index.php
@@ -10,6 +10,10 @@
     <p>Please fill in the fields</p>
     <form action="contact.php" method="post">
         <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="Subject" placeholder="Subject">
+        <textarea name="message" placeholder="Enter message"></textarea>
+        <button type="submit" name="submit">SEND E-MAIL</button>
     </form>
 </body>
 </html>

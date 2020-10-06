b5a14e5278151a5af3f1f4e7cbfd473a2d20d167
rich5k <richardanatsui@gmail.com>
2020-10-06 01:28:31 +0100

    added variables to accept inputs from form and to send an email message from the given email to mine

---
+++ contact/contact.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit'])){
+    $name= trim($_POST['name']);
+    $email= trim($_POST['email']);
+    $subject= trim($_POST['subject']);
+    $message= trim($_POST['message']);
+
+    $myMail= "richardanatsui@gmail.com";
+    $header = "From:". $email;
+    $message2= "You have received a message from
+        ". $name . ". \n\n". $message;
+
+    mail($myMail,$subject, $message2, $header);
+    header("Location: index.php?mailsend");
+}
+?>
